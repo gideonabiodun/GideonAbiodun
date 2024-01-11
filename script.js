f603d3ddcb8491b0f5bdbc4acedebbeb250d1f81
Gideon-Abiodun <skuriangideon@gmail.com>
2024-01-11 12:35:01 +0100

    change descriptions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Auto types function
 
 var typed = new Typed(".auto-input", {
-    strings: ["I'm Gideon <br> Abiodun", " I'm a Web <br> Developer", "I'm a Web <br> Designer"],
+    strings: ["I'm Gideon <br> Abiodun", " I'm a Frontend <br> Developer", "I'm a Web <br> Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
